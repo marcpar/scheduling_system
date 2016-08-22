927c3584e417c77eaa049e3478661e01638ae350
Marc Par <marc.par10@gmail.com>
2016-08-22 12:20:59 +0800

    push project scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name schedulingSystemApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the schedulingSystemApp
+ */
+angular.module('schedulingSystemApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name schedulingSystemApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the schedulingSystemApp
+ */
+angular.module('schedulingSystemApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
